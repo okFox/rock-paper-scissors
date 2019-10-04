609a13f4e53fa398267e7c5b47e1e7397a391cbb
okFox <smayfield@gmail.com>
2019-10-03 20:57:05 -0700

    OMG the game works

--- app.js
+++ app.js
@@ -1,22 +1,53 @@
 // import functions
 import './checkResult.js';
 import './getRandomThrow.js';
+import getRandomThrow from './getRandomThrow.js';
+import checkResult from './checkResult.js';
 
 
 //get DOM elements
-const rockThrow = document.getElementById('rock');
-const paperThrow = document.getElementById('paper');
-const scissorsThrow = document.getElementById('scissors');
+//const rockThrow = document.getElementById('rock');
+//const paperThrow = document.getElementById('paper');
+//const scissorsThrow = document.getElementById('scissors');
 const oppSpan = document.getElementById('opp-throw');
 const gameResultSpan = document.getElementById('game-result');
-const gamesPlayedSpan = Document.getElementById('games-played');
-const gamesWonSpan = document.getElementById('games-won');
+//const gamesPlayedSpan = document.getElementById('games-played');
+const wldSpan = document.getElementById('games-wld');
+const shootButton = document.getElementById('shoot-button');
 //const playAgainButton = document.getElementById();
 
 //initial state
-let gamesPlayed = 0;
-let gamesWon = 0;
+let wld = [0, 0, 0];
+let oppLastThrow;
 
 
-//Random Number Generator   
-let randomNum = Math.floor(Math.random() * 3);
+
+
+
+
+let randomNum = () => Math.floor(Math.random() * 3);
+
+const playGame = () => {
+    const selectedRadioButton = document.querySelector('input:checked');
+    const userSelectedThrow = selectedRadioButton.value;  
+    
+    
+    let compCurrentThrow = getRandomThrow(randomNum);
+    
+    let gameResult = checkResult(userSelectedThrow, compCurrentThrow);
+    gameResultSpan.textContent = gameResult;
+    oppSpan.textContent = compCurrentThrow;
+    
+      
+};
+
+
+//const updateSpans = () => {
+//  gameResultSpan.textContent = gameResult;
+   
+//}
+
+
+
+shootButton.addEventListener('click', playGame);
+
